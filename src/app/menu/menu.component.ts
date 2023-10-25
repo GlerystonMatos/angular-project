7ff868ac18050551a1a92f79ace32f7d2037a76e
Gleryston Matos <glerystonmatos@gmail.com>
2023-10-25 07:52:53 -0300

    Lazy-loading

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -3,19 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 
 export class MenuComponent {
   public texto: string = '';
 
   constructor() { }
-
-  ngOnInit(): void {
-    this.texto = 'Olá tudo bem?';
-  }
-
-  clicou() {
-    this.texto = 'Clicou';
-  }
 }
