a726b0a588f5e0eade67d68e21fd82db4ca68aeb
Gleryston Matos <glerystonmatos@gmail.com>
2023-12-04 08:15:31 -0300

    Formulário simples

--- src/app/usuario/usuario.module.ts
+++ src/app/usuario/usuario.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { UsuarioComponent } from './usuario.component';
 import { UsuarioRoutingModule } from './usuario.routing.module';
+import { UsuarioCadastroComponent } from './cadastro/usuario-cadastro';
 
 @NgModule({
-  declarations: [UsuarioComponent],
+  declarations: [UsuarioComponent, UsuarioCadastroComponent],
   imports: [
+    FormsModule,
     CommonModule,
     UsuarioRoutingModule,
   ]
