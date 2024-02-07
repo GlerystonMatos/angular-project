30d14c32dca63811b59a1d5dd13f2a5bae833744
Gleryston Matos <glerystonmatos@gmail.com>
2024-02-07 19:01:28 -0300

    Table no PrimeNG

--- src/app/usuario/usuario.module.ts
+++ src/app/usuario/usuario.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
+import { TableModule } from 'primeng/table';
 import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 import { CommonModule } from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
 import { UsuarioComponent } from './usuario.component';
 import { UsuarioRoutingModule } from './usuario.routing.module';
 import { UsuarioCadastroComponent } from './cadastro/usuario-cadastro';
@@ -8,8 +11,11 @@ import { UsuarioCadastroComponent } from './cadastro/usuario-cadastro';
 @NgModule({
   declarations: [UsuarioComponent, UsuarioCadastroComponent],
   imports: [
+    TableModule,
     FormsModule,
     CommonModule,
+    ButtonModule,
+    InputTextModule,
     UsuarioRoutingModule,
   ]
 })
